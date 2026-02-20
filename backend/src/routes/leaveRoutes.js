7d6214715022dc2e56068ae29b4b69052b7034a9
bismarajputt098 <bismarajputt203@gmail.com>
2026-02-20 16:21:57 +0500

    Yes

--- backend/src/routes/leaveRoutes.js
+++ backend/src/routes/leaveRoutes.js
@@ -19,3 +19,6 @@ router.get("/my/:employeeId", authorize("Employee"), getMyLeaves);
 router.get("/", authorize("Admin", "HR", "Manager"), getAllLeaves);
 router.put("/:id", authorize("Admin", "HR", "Manager"), updateLeaveStatus);
 module.exports = router;
+
+
+// Yes Committed
