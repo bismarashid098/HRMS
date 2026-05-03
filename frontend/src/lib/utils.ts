e0dbac4ce64527111ac8ba53f34536c46e7894be
bismarajputt098 <bismarajputt203@gmail.com>
2026-05-03 15:15:44 +0500

    Yes

---
+++ frontend/src/lib/utils.ts
@@ -0,0 +1,10 @@
+import React from 'react';
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+/**
+ * Utility for merging tailwind classes safely
+ */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
