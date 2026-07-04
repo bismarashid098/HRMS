795e2eb7ba9ac6cf6753ac0fa8e26841e8393601
bismarajputt098 <bismarajputt203@gmail.com>
2026-07-04 17:55:23 +0500

    yesdd

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,10 @@ app.listen(PORT, () => {
 
 process.on("uncaughtException", (err) => {
   console.error("UNCAUGHT EXCEPTION 💥", err);
+  process.exit(1);
 });
 
 process.on("unhandledRejection", (err) => {
   console.error("UNHANDLED REJECTION 💥", err);
+  process.exit(1);
 });
