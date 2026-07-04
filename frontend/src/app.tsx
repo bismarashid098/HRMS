4cd2c1ada516c5e0f52aecf5fd9e53faf1df2dc8
bismarajputt098 <bismarajputt203@gmail.com>
2026-07-04 21:22:01 +0500

    uiyiy

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,29 +1,14 @@
-import { useEffect, useLayoutEffect } from 'react';
+import { useEffect } from 'react';
 import { Outlet, useLocation } from 'react-router';
-import { useSettingsContext } from 'providers/SettingsProvider';
-import { REFRESH } from 'reducers/SettingsReducer';
-import SettingPanelToggler from 'components/settings-panel/SettingPanelToggler';
-import SettingsPanel from 'components/settings-panel/SettingsPanel';
 
 const App = () => {
   const { pathname } = useLocation();
-  const { configDispatch } = useSettingsContext();
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
 
-  useLayoutEffect(() => {
-    configDispatch({ type: REFRESH });
-  }, []);
-
-  return (
-    <>
-      <Outlet />
-      <SettingsPanel />
-      <SettingPanelToggler />
-    </>
-  );
+  return <Outlet />;
 };
 
 export default App;
